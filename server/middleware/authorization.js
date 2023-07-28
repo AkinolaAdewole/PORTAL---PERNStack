1a91218494b7a36505384dece4757d44f6874f23
Akinola <adewoleakinola24@gmail.com>
2023-07-28 23:11:27 +0100

    Token generation

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,6 +1,14 @@
+const jwt =require("jsonwebtoken");
+
+
 module.exports = async(req,res,next)=>{
     try {
         const jwtToken = req.header("token");
+        if(!jwtToken){
+            return res.status(403).json("You are authorised");
+        }
+
+        const payload
     } catch (error) {
         console.error(error.message);
         return res.status(403).json("You are not authorised");
