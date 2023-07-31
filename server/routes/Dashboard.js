5f3d44bfe3dcad2829bab109ddd56da12b7dea34
Akinola <adewoleakinola24@gmail.com>
2023-07-31 16:27:47 +0100

    client side

--- server/routes/Dashboard.js
+++ server/routes/Dashboard.js
@@ -4,7 +4,7 @@ const Dashboard=async(req,res)=>{
     try {
         // req.user has the payload
         const user = executeQuery("SELECT user_name FROM users WHERE user_id=$1",[req.user]);
-        res.status(200).json(user[0]);
+        res.status(200).json(user[0]); 
         // res.json(user[0]);
     } catch (err) {
         console.error(err);
