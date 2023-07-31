215babcc713c87e6b8bdeb8586a5423399cf3a93
Akinola <adewoleakinola24@gmail.com>
2023-07-31 21:03:30 +0100

    client side signUp authentication

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -16,27 +16,42 @@ const SignUp = () => {
 
     const submit =async(e)=>{
         e.preventDefault();
+        try {
+            const body ={firstname, lastname, email, password }
+            const response = await fetch("http://localhost:5018/register",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(body)
+            });
+            const parseRes=await response.json();
+            console.log(parseRes);
+            
+        } catch (err) {
+            console.error(err.message);
+        }
     }
 
   return (
     <>
        <h1 className='text-center my-5'> Sign Up</h1>
        <div className="container">
-        <form action="">
-            <input type="text" className="form-control my-3" name='firstname' 
-            placeholder='Name' value={firstname} onChange={(e)=>onChange(e)} />
+           <div className='col-9'>
+                <form action="" onSubmit={submit}>
+                    <input type="text" className="form-control my-3" name='firstname' 
+                    placeholder='Name' value={firstname} onChange={(e)=>onChange(e)} />
 
-            <input type="text" className="form-control my-3" name='lastname' 
-            placeholder='Name' value={lastname} onChange={(e)=>onChange(e)}/> 
+                    <input type="text" className="form-control my-3" name='lastname' 
+                    placeholder='Name' value={lastname} onChange={(e)=>onChange(e)}/> 
 
-            <input type="email" className="form-control my-3" name='email' 
-            placeholder='Email' value={email} onChange={(e)=>onChange(e)}/>
+                    <input type="email" className="form-control my-3" name='email' 
+                    placeholder='Email' value={email} onChange={(e)=>onChange(e)}/>
 
-            <input type="password" className="form-control my-3" name='password' 
-            placeholder='Password' value={password} onChange={(e)=>onChange(e)}/>
+                    <input type="password" className="form-control my-3" name='password' 
+                    placeholder='Password' value={password} onChange={(e)=>onChange(e)}/>
 
-            <button className="btn btn-success btn-block">Submit</button>
-        </form>
+                    <button className="btn btn-success btn-block">Submit</button>
+                </form>
+           </div>
        </div>
     </>
   )
