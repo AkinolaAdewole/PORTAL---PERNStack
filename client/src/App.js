57e3d514b546a7b312a23eb612eb1b90b5324417
Akinola <adewoleakinola24@gmail.com>
2023-07-31 18:45:40 +0100

    client side verification

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import { useState } from 'react';
 
 function App() {
+  const [isAunthenticated, setIsAuthenticated]= useState();
   return (
-    <div className="App">
-     
+    <div className='container'>
+      <Router>
+         <Routes>
+           <Route path='/' element={<SignUp />}/>
+           <Route path='/login' element={<Login />} />
+           <Route path='/dashboard' element={<Dashboard />} />
+         </Routes>
+      </Router>
     </div>
   );
 }
