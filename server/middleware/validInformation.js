03840159759accc29f033b933908d2201ff0a2f4
Akinola <adewoleakinola24@gmail.com>
2023-07-31 08:52:28 +0100

    JWT verification

--- server/middleware/validInformation.js
+++ server/middleware/validInformation.js
@@ -1,4 +1,22 @@
 module.exports=(req,res,next)=>{
     const {email, name, password}=req.body;
-    const validEmail=(userEmail)=>{}
+
+    // Check if Email is valid
+    const validEmail=(userEmail)=>{
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+    }
+
+    if(req.path === "/register"){
+        console.log(!email.length);
+
+        // email, name and password are not empty value
+        if(![email,name,password].every(Boolean)){
+            return res.status(401).json("Missing Credentials");
+        } else if(!validEmail(email)){
+            return res.json("Invalid Email")
+        }
+
+    } else if(req.path === "/login"){
+        if(![email,password].every(Boolean)){}
+    }
 }
