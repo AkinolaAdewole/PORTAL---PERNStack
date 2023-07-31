3a40acbf657f47871509e9aa89d63d1ac07f9308
Akinola <adewoleakinola24@gmail.com>
2023-07-31 16:45:26 +0100

    client side

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Login = ({setAuth}) => {
+    const [inputs, setInputs]=useState({
+        email:"",
+        password:""
+    });
+
+    const {email,password}=inputs;
+
+    const onChange=(e)=>{
+        setInputs({...inputs, [e.target.name]:e.target.value});
+    }
+
+    const submitForm=async(e)=>{
+        e.preventDefault();
+        try {
+            const body={email,password};
+            const response = await fetch(
+                "http://localhost:5018/login",
+                {
+                    method:"POST",
+                    headers:{
+                        "content-type":"application/json"
+                    },
+                    body:JSON.stringify(body);
+                }
+            )
+        } catch (err) {
+            console.error(err);
+        }
+    }
+  return (
+    <>
+    </>
+  )
+}
+
+export default Login
