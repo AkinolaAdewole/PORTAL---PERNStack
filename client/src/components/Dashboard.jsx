855bf7de2b978865beca203345b35d4a27113686
Akinola <adewoleakinola24@gmail.com>
2023-08-02 23:08:31 +0100

    Login successful

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,12 +6,13 @@ const Dashboard = ({setAuth}) => {
 
     const getProfile=async()=>{
         try {
-            const res = await fetch ("http://localhost:5000/dashboard",{
-                method:"POST",
+            const res = await fetch ("http://localhost:5020/dashboard",{
+                method:"GET",
                 headers:{jwt_token:localStorage.token}
             });
             const parseData = await res.json();
             setName(parseData.user_name);
+
         } catch (err) {
             console.error(err.message);
         }
