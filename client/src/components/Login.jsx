4f97da6191a0ef5758808014dd2f54e29ab801f8
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-04 23:29:35 +0100

    Display an error message if the credentials are wrong or if there's a server error

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,74 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 
-const Login = ({setAuth}) => {
-    const [inputs, setInputs]=useState({
-        email:"",
-        password:""
+const Login = ({ setAuth }) => {
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
     });
 
-    const {email,password}=inputs;
+    const { email, password } = inputs;
 
-    const onChange=(e)=>{
-        setInputs({...inputs, [e.target.name]:e.target.value});
-    }
+    const onChange = (e) => {
+        setInputs({ ...inputs, [e.target.name]: e.target.value });
+    };
 
-    const submitForm=async(e)=>{
+    const submitForm = async (e) => {
         e.preventDefault();
         try {
-            const body={email,password};
+            if (!email || !password) {
+                toast.error("Please fill in all fields");
+                return;
+            }
+
+            const body = { email, password };
 
             const response = await fetch(
                 "http://localhost:5020/login",
                 {
-                    method:"POST",
-                    headers:{
-                        "Content-Type":"application/json"
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
                     },
-                    body:JSON.stringify(body)
+                    body: JSON.stringify(body)
                 }
             );
 
-            const parseRes=await response.json();
-            console.log(parseRes);
-                localStorage.setItem("token",parseRes.jwtToken);
+            const parseRes = await response.json();
+
+            if (parseRes.jwtToken) {
+                localStorage.setItem("token", parseRes.jwtToken);
                 setAuth(true);
-                toast.success("Logged in successfuly");
-         
+                toast.success("Logged in successfully");
+            } else {
+                toast.error(parseRes.message || "Invalid credentials");
+            }
+
         } catch (err) {
             console.error(err);
+            toast.error("Server error. Please try again later.");
         }
-    }
-  return (
-    <>
-       <div>
-            <div>
-                <div className='col-9 shadow-sm'>
-                     <h2 className='text-center'>Login</h2>
-                    <form action="" onSubmit={submitForm}>
-                        <input type="text" name='email' 
-                            placeholder='Email'
-                            className='form-control my-3' 
-                            value={email}
-                            onChange={(e)=>onChange(e)}/>
+    };
 
-                        <input type="text" name='password' 
-                           placeholder='password'
-                            className='form-control my-3' 
-                            value={password}
-                            onChange={(e)=>onChange(e)} 
-                            />
-
-                        <button className='btn btn-success'>Login</button>
-                    </form>
-
-                    <Link to='/'>Sign Up</Link>
-                </div>
+    return (
+        <div>
+            <div className="col-9 shadow-sm">
+                <h2 className="text-center">Login</h2>
+                <form onSubmit={submitForm}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="form-control my-3"
+                        value={email}
+                        onChange={onChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        className="form-control my-3"
+                        value={password}
+                        onChange={onChange}
+                    />
+                    <button className="btn btn-success">Login</button>
+                </form>
+                <Link to='/'>Sign Up</Link>
             </div>
-       </div>
-    </>
-  )
-}
+        </div>
+    );
+};
 
-export default Login 
+export default Login;
